ce984e1064acddabc3cb09ea992b0cf5698fd9e5
Ilya Nikolaev <rrypmk3uvoc8@mail.ru>
2021-12-03 02:26:35 +0300

    🎨 BotAdminFilter refactored

--- app/filters/bot_admin_filter.py
+++ app/filters/bot_admin_filter.py
@@ -7,13 +7,8 @@ from app import load_config
 
 
 class BotAdminFilter(Filter):
-    
     def __init__(self):
         self.config = load_config()
     
     async def check(self, u: Union[types.Message, types.CallbackQuery, types.InlineQuery]) -> bool:
-        
-        if u.from_user.id in self.config.bot_settings.admins:
-            return True
-        
-        return False
+        return u.from_user.id in self.config.bot_settings.admins
