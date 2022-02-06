f92bf30f9111b27857e8270f449a6ff0ea3b25b5
Ilya Nikolaev <rrypmk3uvoc8@mail.ru>
2022-02-06 17:15:57 +0300

    🔥 Removed repr

--- app/db_api/models.py
+++ app/db_api/models.py
@@ -1,19 +1,16 @@
-from sqlalchemy import Column
 from sqlalchemy import BigInteger, Text, Boolean, DateTime
+from sqlalchemy import Column
 from sqlalchemy import false
 from sqlalchemy.sql.functions import now
 
-from .base import Base
+from app.db_api.base import Base
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     
     tg_id = Column(BigInteger, primary_key=True)
 
     username = Column(Text, unique=True)
     banned = Column(Boolean, nullable=False, default=false())
     created_at = Column(DateTime, nullable=False, server_default=now())
-    
-    def __repr__(self):
-        return f"User({self.tg_id}, {self.username}, {self.banned})"
