06ec20495d7f070cc943552fd98dcafaadf7505c
Ilya Nikolaev <rrypmk3uvoc8@mail.ru>
2022-02-06 17:12:31 +0300

    🎨 Improve format of the code.

--- app/db_api/models.py
+++ app/db_api/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, BIGINT, TEXT, BOOLEAN, false
+from sqlalchemy import Column
+from sqlalchemy import BigInteger, Text, Boolean, DateTime
+from sqlalchemy import false
+from sqlalchemy.sql.functions import now
 
 from .base import Base
 
@@ -6,10 +9,11 @@ from .base import Base
 class User(Base):
     __tablename__ = 'users'
     
-    tg_id = Column(BIGINT, primary_key=True)
-    username = Column(TEXT, nullable=True, unique=True)
-    
-    banned = Column(BOOLEAN, nullable=False, default=false())
+    tg_id = Column(BigInteger, primary_key=True)
+
+    username = Column(Text, unique=True)
+    banned = Column(Boolean, nullable=False, default=false())
+    created_at = Column(DateTime, nullable=False, server_default=now())
     
     def __repr__(self):
         return f"User({self.tg_id}, {self.username}, {self.banned})"
