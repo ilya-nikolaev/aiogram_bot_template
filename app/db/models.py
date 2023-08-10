1957a1efff4a44d5d0deae7d27ef486cb923f200
Ilya Nikolaev <65247719+ilya-nikolaev@users.noreply.github.com>
2023-08-11 01:09:57 +0300

    Base and models ported to SQLAlchemy2.0

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import BigInteger, Text, Boolean, DateTime
-from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import BIGINT, TEXT, BOOLEAN, TIMESTAMP
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped
+
 from sqlalchemy import false
 from sqlalchemy.sql.functions import now
 
@@ -9,8 +11,10 @@ from app.db.base import Base
 class User(Base):
     __tablename__ = "users"
     
-    tg_id = Column(BigInteger, primary_key=True)
+    tg_id: Mapped[int] = mapped_column(BIGINT, primary_key=True)
 
-    username = Column(Text, unique=True)
-    banned = Column(Boolean, nullable=False, default=false())
-    created_at = Column(DateTime, nullable=False, server_default=now())
+    username: Mapped[str] = mapped_column(TEXT, nullable=True, unique=True)
+
+    banned: Mapped[bool] = mapped_column(BOOLEAN, nullable=False, default=false())
+
+    created_at = mapped_column(TIMESTAMP, nullable=False, server_default=now())
